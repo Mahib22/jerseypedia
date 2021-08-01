965af6348e6c0b1e8cb532385211f84b1aac5d6e
Mahib22 <mahib.arib@gmail.com>
2021-08-01 21:19:15 +0700

    membuat halaman history

--- app/Http/Livewire/History.php
+++ app/Http/Livewire/History.php
@@ -3,11 +3,19 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Order;
 
 class History extends Component
 {
+    public $orders;
+
     public function render()
     {
+        if (Auth::user()) {
+            $this->orders = Order::where('user_id', Auth::user()->id)->where('status', '!=', 0)->get();
+        }
+
         return view('livewire.history');
     }
 }
