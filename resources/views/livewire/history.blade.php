9d53b0a52255d350840993130f768fda734d3195
Mahib22 <mahib.arib@gmail.com>
2021-08-02 14:27:30 +0700

    membuat halaman history

--- resources/views/livewire/history.blade.php
+++ resources/views/livewire/history.blade.php
@@ -19,4 +19,74 @@
             @endif
         </div>
     </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td>No.</td>
+                            <td>Tanggal Pesanan</td>
+                            <td>Kode Pemesanan</td>
+                            <td>Pesanan</td>
+                            <td>Status</td>
+                            <td><strong>Total Harga</strong></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = 1 ?>
+                        @forelse ($orders as $order)
+                            <tr>
+                                <td>{{ $no++ }}</td>
+                                <td>{{ $order->created_at }}</td>
+                                <td>{{ $order->order_code }}</td>
+                                <td>
+                                    @php
+                                        $order_details = \App\OrderDetail::where('order_id', $order->id)->get();
+                                    @endphp
+                                    @foreach ($order_details as $order_detail)
+                                        <img class="img-fluid" src="{{ url('assets/jersey') }}/{{ $order_detail->product->img }}" width="50" alt="{{ $order_detail->product->name }}">
+                                        {{ $order_detail->product->name }}
+                                        <br>
+                                    @endforeach
+                                </td>
+                                <td>
+                                    @if ($order->status == 1)
+                                        Belum Lunas
+                                    @else
+                                        Lunas
+                                    @endif
+                                </td>
+                                <td><strong>
+                                    Rp {{ number_format($order->total_price) }}
+                                </strong></td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="7" class="text-center">Tidak ada history</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-3">
+        <div class="col">
+            <div class="card shadow">
+                <div class="card-body">
+                    <p>Untuk pembayaran silahkan transfer di rekening dibawah ini : </p>
+                    <div class="media">
+                        <img src="{{ url('assets/bri.png') }}" class="mr-3" alt="Logo BRI" width="60">
+                        <div class="media-body">
+                            <h5 class="mt-0">BANK BRI</h5>
+                            <p>No. Rekening <strong>012345-678-910</strong> atas nama <strong>Ucok</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
