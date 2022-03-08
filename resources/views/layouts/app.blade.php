d6b9729183a1a848477a2836b61fd4e07ca6cd91
Mahib22 <mahib.arib@gmail.com>
2022-03-08 08:34:29 +0700

    integrasi template adminlte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,21 +10,20 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap"
+        rel="stylesheet">
     <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/adminlte.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-    
+
     <!-- Livewire Style -->
     @livewireStyles
 </head>
+
 <body>
     <div id="app">
         @livewire('navbar')
@@ -37,5 +37,9 @@
 
     <!-- Livewire Script -->
     @livewireScripts
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/adminlte.js') }}"></script>
 </body>
+
 </html>
--- resources/views/livewire/product-detail.blade.php
+++ resources/views/livewire/product-detail.blade.php
@@ -44,24 +44,59 @@
                 @endif
             </h4>
             <h3>Rp {{ number_format($product->price) }}</h3>
-            <h4>Berat : {{ $product->weight }}</h4>
-            <h4>Deskripsi Produk :</h4>
-            <h5>{{ $product->description }}</h5>
+
+            <ul class="nav nav-tabs mt-4" id="myTab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="info-tab" data-bs-toggle="tab" data-bs-target="#info"
+                        type="button" role="tab" aria-controls="info" aria-selected="true">Info Produk</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="desc-tab" data-bs-toggle="tab" data-bs-target="#desc"
+                        type="button" role="tab" aria-controls="desc" aria-selected="false">Deskripsi</button>
+                </li>
+            </ul>
+            <div class="tab-content mt-2" id="myTabContent">
+                <div class="tab-pane fade show active" id="info" role="tabpanel" aria-labelledby="info-tab">
+                    <p>Kondisi : Baru</p>
+                    <p>Berat : {{ $product->weight }}</p>
+                    <p>Kategori : Jersey Bola Pria</p>
+                    <p>Jersey Liga : La Liga</p>
+                    <p>Size tersedia : M, L, XL</p>
+                    <p>Stok tersedia : 10</p>
+                </div>
+                <div class="tab-pane fade" id="desc" role="tabpanel" aria-labelledby="desc-tab">
+                    <h5>{{ $product->description }}</h5>
+                </div>
+            </div>
         </div>
 
         <div class="col-md-3">
-            <div class="card">
-                <div class="card-header">Jumlah Pesanan</div>
+            <div class="card shadow-sm">
                 <div class="card-body">
                     <form wire:submit.prevent="addToCart">
-                        <div class="input-group mb-3">
-                            <input type="number" class="form-control @error('order_quantity') is-invalid @enderror"
+                        <div class="mb-3">
+                            <label for="size" class="form-label">Pilih Ukuran</label>
+                            <select class="form-control" id="size">
+                                <option value="1">M</option>
+                                <option value="2">L</option>
+                                <option value="3">XL</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="jumlahPesanan" class="form-label">Jumlah Pesanan</label>
+                            <input id="jumlahPesanan" type="number"
+                                class="form-control @error('order_quantity') is-invalid @enderror"
                                 wire:model="order_quantity" required min="1">
                             @error('order_quantity')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="note" class="form-label">Tambah Catatan</label>
+                            <input type="text" class="form-control" placeholder="Contoh: Size M alternate L"
+                                id="note">
                         </div>
                         <button type="submit" class="btn btn-primary btn-block"
                             @if ($product->is_ready !== 1) disabled @endif><i class="fas fa-plus mr-2"></i> Tambah ke
@@ -71,22 +106,21 @@
                     @auth
                         <form wire:submit.prevent="addToWishlist">
                             @if (Auth::user()->wishlists()->where('product_id', $product->id)->first())
-                                <button type="submit" class="btn btn-danger btn-block rounded-pill"><i
-                                        class="fas fa-heart mr-2"></i>
+                                <button type="submit" class="btn btn-danger btn-block"><i class="fas fa-heart mr-2"></i>
                                     Hapus dari Wishlist</button>
                             @else
-                                <button type="submit" class="btn btn-success btn-block rounded-pill"><i
-                                        class="far fa-heart mr-2"></i>
+                                <button type="submit" class="btn btn-success btn-block"><i class="far fa-heart mr-2"></i>
                                     Tambah ke Wishlist</button>
                             @endif
                         </form>
                     @else
                         <form wire:submit.prevent="addToWishlist">
-                            <button type="submit" class="btn btn-success btn-block rounded-pill"><i
-                                    class="far fa-heart mr-2"></i>
+                            <button type="submit" class="btn btn-success btn-block"><i class="far fa-heart mr-2"></i>
                                 Tambah ke Wishlist</button>
                         </form>
                     @endauth
+                    <button class="btn btn-outline-secondary btn-block mt-2"><i class="fas fa-share-alt mr-2"></i>
+                        Bagikan Link</button>
                 </div>
             </div>
         </div>
