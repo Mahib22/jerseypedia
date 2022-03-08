369871d290a417b84abe2966eaf8ac2ae191d471
Mahib22 <mahib.arib@gmail.com>
2022-03-08 21:35:38 +0700

    Revert "integrasi template adminlte"
    This reverts commit d6b9729183a1a848477a2836b61fd4e07ca6cd91.

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -3,8 +3,8 @@
         <div class="col">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-dark">Home</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Keranjang</li>
+                  <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-dark">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Keranjang</li>
                 </ol>
             </nav>
         </div>
@@ -12,7 +12,7 @@
 
     <div class="row">
         <div class="col-md-12">
-            @if (session()->has('message'))
+            @if(session()->has('message'))
                 <div class="alert alert-danger">
                     {{ session('message') }}
                 </div>
@@ -36,14 +36,12 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php $number = 1; ?>
+                        <?php $number = 1 ?>
                         @forelse ($order_details as $order_detail)
                             <tr>
                                 <td>{{ $number++ }}</td>
                                 <td>
-                                    <img class="img-fluid"
-                                        src="{{ url('assets/jersey') }}/{{ $order_detail->product->img }}"
-                                        width="200" alt="{{ $order_detail->product->name }}">
+                                    <img class="img-fluid" src="{{ url('assets/jersey') }}/{{ $order_detail->product->img }}" width="200" alt="{{ $order_detail->product->name }}">
                                 </td>
                                 <td>
                                     {{ $order_detail->product->name }}
@@ -55,11 +53,10 @@
                                     Rp {{ number_format($order_detail->product->price) }}
                                 </td>
                                 <td><strong>
-                                        Rp {{ number_format($order_detail->total_price) }}
+                                    Rp {{ number_format($order_detail->total_price) }}
                                 </td></strong>
                                 <td>
-                                    <i wire:click="destroy({{ $order_detail->id }})"
-                                        class="fas fa-trash text-danger"></i>
+                                    <i wire:click="destroy({{ $order_detail->id }})" class="fas fa-trash text-danger"></i>
                                 </td>
                             </tr>
                         @empty
@@ -67,33 +64,33 @@
                                 <td colspan="7" class="text-center">Keranjang Kosong</td>
                             </tr>
                         @endforelse
-
+                        
                         @if (!empty($order))
                             <tr>
                                 <td colspan="5" align="right"><strong>Total Harga : </strong></td>
                                 <td align="left"><strong>
-                                        Rp {{ number_format($order->total_price) }}
+                                    Rp {{ number_format($order->total_price) }}
                                 </td></strong>
                                 <td></td>
                             </tr>
                             <tr>
                                 <td colspan="5" align="right"><strong>Kode Unik : </strong></td>
                                 <td align="left"><strong>
-                                        Rp {{ number_format($order->unique_code) }}
+                                    Rp {{ number_format($order->unique_code) }}
                                 </td></strong>
                                 <td></td>
                             </tr>
                             <tr>
                                 <td colspan="5" align="right"><strong>Total yang harus dibayar : </strong></td>
                                 <td align="left"><strong>
-                                        Rp {{ number_format($order->total_price + $order->unique_code) }}
+                                    Rp {{ number_format($order->total_price + $order->unique_code) }}
                                 </td></strong>
                                 <td></td>
                             </tr>
                             <tr>
                                 <td colspan="5"></td>
                                 <td colspan="3">
-                                    <a href="{{ route('checkout') }}" class="btn btn-success btn-block radius-10">
+                                    <a href="{{ route('checkout') }}" class="btn btn-success btn-block">
                                         <i class="fas fa-shopping-cart"></i> Check Out
                                     </a>
                                 </td>
